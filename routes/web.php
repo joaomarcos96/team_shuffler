4e81c471ac50ffa8ec63e3ba2b822c6abea91039
Joao Marcos <jao.markos@hotmail.com>
2020-10-17 14:22:41 -0300

    Add route /players as default route /

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [PlayerController::class, 'index']);
+
 Route::resource('players', PlayerController::class)->except(['show']);

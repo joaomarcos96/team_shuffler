20c7e7ec3cb41c4cdd15aa12beb372715b97cb04
Joao Marcos <jao.markos@hotmail.com>
2020-10-20 21:55:03 -0300

    Assign players to teams

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Player;
+use App\Utils\ArrayUtils;
+use ArrayIterator;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Redirect;
@@ -14,9 +17,9 @@ class TeamController extends Controller
      * @param Request $request
      * @return Response
      */
-    public function store(Request $request)
+    public function store()
     {
-        $confirmedPlayersId = array_keys($request->get('confirm') ?? []);
+        $confirmedPlayersId = array_keys(request('confirm') ?? []);
 
         if (empty($confirmedPlayersId) || !is_array($confirmedPlayersId) || !count($confirmedPlayersId)) {
             Session::flash('message', 'Confirm players to be able to form teams');
@@ -24,7 +27,7 @@ class TeamController extends Controller
             return Redirect::route('players.index');
         }
 
-        $numberOfPlayersPerTeam = (int) $request->get('players-per-team');
+        $numberOfPlayersPerTeam = (int) request('players-per-team');
         $numberOfPlayers = count($confirmedPlayersId);
 
         if ($numberOfPlayers < $numberOfPlayersPerTeam * 2) {
@@ -34,12 +37,16 @@ class TeamController extends Controller
         }
 
         $confirmedPlayers = Player::getConfirmedPlayers($confirmedPlayersId);
+
+        $outfieldPlayers = $confirmedPlayers->reject(function ($player) {
+            return $player->goalkeeper;
+        });
         $goalkeepers = $confirmedPlayers->filter(function ($player) {
             return $player->goalkeeper;
         });
 
         $numberOfGoalkeepers = $goalkeepers->count();
-        $numberOfOutfieldPlayers = $numberOfPlayers - $numberOfGoalkeepers;
+        $numberOfOutfieldPlayers = $outfieldPlayers->count();
 
         $numberOfTeams = $this->getNumberOfTeams($numberOfPlayersPerTeam, $numberOfOutfieldPlayers, $numberOfGoalkeepers);
         if ($numberOfTeams < 2) {
@@ -48,28 +55,82 @@ class TeamController extends Controller
             return Redirect::route('players.index');
         }
 
-        // Group by level
-        $playersGroupedByLevel = [];
-        foreach ($confirmedPlayers as $player) {
-            $playersGroupedByLevel[$player->level][] = $player;
-        }
+        $teams = $this->assignGoalkeepersToTeams($goalkeepers, $numberOfTeams);
 
-        // Shuffle
-        foreach ($playersGroupedByLevel as $players) {
-            shuffle($players);
-        }
+        $this->assignOutfieldPlayersToTeams($outfieldPlayers, $teams, $numberOfPlayersPerTeam);
+
+        dd($teams);
 
         return Redirect::route('players.index');
     }
 
-    /**
-     * @param int $numberOfPlayersPerTeam
-     * @param int $numberOfOutfieldPlayers
-     * @param int $numberOfGoalkeepers
-     * @return int
-     */
-    private function getNumberOfTeams($numberOfPlayersPerTeam, $numberOfOutfieldPlayers, $numberOfGoalkeepers)
+    private function assignGoalkeepersToTeams(Collection $goalkeepers, int $numberOfTeams): array
     {
+        $teams = array_fill(0, $numberOfTeams, null);
+
+        $goalkeepersGroupedByLevel = $goalkeepers->groupBy('level');
+
+        $shuffledGoalkeepers = ArrayUtils::shuffleEveryElement($goalkeepersGroupedByLevel->toArray());
+
+        $goalkeepersArray = array_values(collect($shuffledGoalkeepers)->collapse()->toArray());
+
+        $assignableSize = min($numberOfTeams, count($goalkeepersArray));
+
+        for ($i = 0; $i < $assignableSize; $i++) {
+            $teams[$i][] = $goalkeepersArray[$i];
+        }
+
+        return $teams;
+    }
+
+    private function assignOutfieldPlayersToTeams(
+        Collection $outfieldPlayers,
+        array &$teams,
+        int $numberOfPlayersPerTeam
+    ) {
+        $outfieldPlayersGroupedByLevel = $outfieldPlayers->groupBy('level');
+
+        $shuffledOutfieldPlayers = ArrayUtils::shuffleEveryElement($outfieldPlayersGroupedByLevel->toArray());
+
+        $outfieldPlayersArray = array_values(collect($shuffledOutfieldPlayers)->collapse()->toArray());
+
+        $numberOfTeams = count($teams);
+        $numberOfOutfieldPlayers = count($outfieldPlayersArray);
+
+        for ($i = 0; $i < $numberOfOutfieldPlayers; $i++) {
+            $team = $teams[$i % $numberOfTeams];
+
+            if (empty($team) || count($team) < $numberOfPlayersPerTeam) {
+                $teams[$i % $numberOfTeams][] = $outfieldPlayersArray[$i];
+            } else {
+                $teamIndex = 0;
+                while ($teamIndex < count($teams) && count($teams[$teamIndex]) >= $numberOfPlayersPerTeam) {
+                    $teamIndex++;
+                }
+                $teams[$teamIndex][] = $outfieldPlayersArray[$i];;
+            }
+
+            unset($outfieldPlayersArray[$i]);
+        }
+
+        // for ($i = $numberOfTeams - 2; $i >= 0; $i--) {
+        //     $lastTeam = $teams[$numberOfTeams - 1];
+
+        //     $removedFirstPlayerOfTheLastTeam = $lastTeam[0];
+
+        //     unset($lastTeam[0]);
+
+        //     if (count($teams[$i]) < $numberOfPlayersPerTeam) {
+        //         $teams[$i][] = $removedFirstPlayerOfTheLastTeam;
+        //     }
+        // }
+    }
+
+    private function getNumberOfTeams(
+        int $numberOfPlayersPerTeam,
+        int $numberOfOutfieldPlayers,
+        int $numberOfGoalkeepers
+    ): int {
         $numberOfTeams = 0;
 
         while ($numberOfOutfieldPlayers > 0) {
